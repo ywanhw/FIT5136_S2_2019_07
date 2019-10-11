201b6d597ea755900b5cb13a3198080d36760968
Adrian <spyip3@student.monash.edu>
2019-10-11 11:14:50 +1100

    Controller included

---
+++ FIT5136_team7_PrimeEvent/src/PrimeEvent.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class PrimeEvent {
+	
+	public PrimeEvent() {
+		
+	}
+
+	public boolean login(String userName, String password) {
+		Boolean valid = false;
+		System.out.println("Testing");
+		
+		return valid;
+		
+	}
+	
+	public boolean createUser(String[] userData) {
+		Boolean valid = false;
+		System.out.println("Testing");
+		
+		return valid;
+	}
+
+	public void logout() {
+		System.out.println("Testing");
+		
+	}
+	
+	public void searchHall(){
+		System.out.println("You can search by name,size,or price");
+		System.out.println("Name press 1");
+		System.out.println("Size press 2");
+		System.out.println("Price press 3");
+		
+		Scanner input = new Scanner(System.in);
+		Hall hallarr[] = new Hall[100];
+		int choice = input.nextInt();
+		
+		if (choice == 1) {
+			System.out.println("Enter the name");
+		    String name = input.next();
+		    for(int i = 0;i<hallarr.length;i++) {
+		    	if(hallarr[i].getName().equals(name)) {
+		    		System.out.println(hallarr[i]);
+		    	}
+		    	else
+		    		System.out.println("Not found");
+		    }
+		}
+		if (choice == 2) {
+			System.out.println("Enter the size");
+			String size = input.next();
+			for(int i = 0;i<hallarr.length;i++) {
+		    	if(hallarr[i].getName().equals(size)) {
+		    		System.out.println(hallarr[i]);
+		    	}
+		    	else
+		    		System.out.println("Not found");
+		    }
+		}
+		if (choice == 3) {
+			System.out.println("Enter the price");
+			String price = input.next();
+			for(int i = 0;i<hallarr.length;i++) {
+		    	if(hallarr[i].getName().equals(price)) {
+		    		System.out.println(hallarr[i]);
+		    	}
+		    	else
+		    		System.out.println("Not found");
+		    }
+		}
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
